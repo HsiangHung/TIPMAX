096683e546bb694286c2f80bd4eefe5de4575757
HsiangHung <hhhung@physics.utexas.edu>
2016-02-15 19:21:01 -0500

    Update sparkjob.sh

--- spark_streaming/sparkjob.sh
+++ spark_streaming/sparkjob.sh
@@ -1,5 +1,4 @@
-#spark-submit --jars jars/spark-streaming-kafka-assembly_2.10-1.6.0.jar --master spark://ip-172-31-2-134:7077  $1 localhost:2181 $2
-
+#!/bin/bash 
 spark-submit --jars jars/spark-streaming-kafka-assembly_2.10-1.6.0.jar --master spark://ip-172-31-2-134:7077  --packages TargetHolding/pyspark-cassandra:0.2.4 --conf spark.cassandra.connection.host=172.31.2.134  $1  localhost:2181 $2 
 
 
