86d34c1f998c7f466683b0ccb7c35723a9942eb9
HsiangHung <hhhung@physics.utexas.edu>
2016-02-23 11:59:30 -0500

    Update ingestion.sh

--- data_ingestion/ingestion.sh
+++ data_ingestion/ingestion.sh
@@ -1,7 +1,6 @@
-#spark-submit --jars jars/spark-streaming-kafka-assembly_2.10-1.6.0.jar --master spark://ip-172-31-2-134:7077  $1 localhost:2181 $2
+
 
 python tick_kafkaproducer.py  52.71.0.86  2009-02-12.csv  1
 
-#spark-submit --jars jars/spark-streaming-kafka-assembly_2.10-1.6.0.jar --master spark://ip-172-31-2-134:7077  --packages TargetHolding/pyspark-cassandra:0.2.4 --conf spark.cassandra.connection.host=172.31.2.134  $1  localhost:2181 $2 
 
 
